597b643ad4bd4d1b9a7926f173625ab721934b34
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-06 15:15:31 -0700

    6 files
    6 files for this directory, in Python (*.py) format.

---
+++ MEDOS HDD Beta/Build 5/1 System/Debug/testconfirm.py
@@ -0,0 +1,6 @@
+testconfirm = int(input("Did the command look right? type 1 to confirm, type anything else and/or press enter to quit "))
+if testconfirm == 1:
+    print ("Command test successful!")
+    print ("Shutting down, please wait")
+else:
+    print ("Shutting down")
