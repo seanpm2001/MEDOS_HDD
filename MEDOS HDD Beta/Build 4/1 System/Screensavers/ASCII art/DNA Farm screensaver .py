95e4cccdb801650d69c3b4d592a83b476ad49b8a
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-06 13:54:20 -0700

    3 ASCII art screensavers
    3 ASCII art screensavers files, in Python (*.py) format for this directory.

---
+++ MEDOS HDD Beta/Build 4/1 System/Screensavers/ASCII art/DNA Farm screensaver .py
@@ -0,0 +1,24 @@
+in1 = str
+print ("Opening screensaver: DNA FARM")
+print ("Type 'OPEN' to try out the screensaver!")
+in1 = str(input("FC://"))
+if in1 == ("OPEN"):
+	x = 1
+	while x == 1:
+		print ("|-----------------|")
+		print ("\---------------/  ")
+		print (" ~-_---------_-~   ")
+		print ("   ~-_---_-~       ")
+		print ("      ~-_		   ")
+		print ("    _-~---~-_      ")
+		print ("  _-~---------~-_  ")
+		print (" /---------------\ ")
+		print ("|-----------------|")
+		x + 0
+testconfirm = int(input("Did the setup look right? type 1 to confirm, type anything else and/or press enter to quit "))
+if testconfirm == 1:
+    print ("System setup successful!")
+    print ("Shutting down, please wait")
+else:
+    print ("Shutting down")
+    
