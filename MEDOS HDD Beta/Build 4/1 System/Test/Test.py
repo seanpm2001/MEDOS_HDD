ce4c5b132472e8fcff1a56af340fb1063ca704bf
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-06 14:07:11 -0700

    5 files
    5 files for this directory, in plain text (2x *.txt) and Python (3x *.py) formats.

---
+++ MEDOS HDD Beta/Build 4/1 System/Test/Test.py
@@ -0,0 +1,17 @@
+y = int
+y == 1
+count = int
+masinput = str
+while y == 1:
+	masinput = str(input("FC:\\"))
+# 8 bit integer counting command script (1 by one)
+if input == ("FC:\\8BITTEST1X"):
+	count = 0
+	count = int
+	count =+ 1
+	print (count)
+while not (count > 256):
+    count += 1
+    print (count)
+else:
+        print ("You have successfully counted to a 8 bit integer")
