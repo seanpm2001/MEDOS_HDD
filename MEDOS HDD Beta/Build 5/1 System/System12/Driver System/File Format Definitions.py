73d749faa78aa15d95ac4aac107cd159084e0a81
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-06 15:20:50 -0700

    4 files
    4 files for this directory, in Python (*.py) format.

---
+++ MEDOS HDD Beta/Build 5/1 System/System12/Driver System/File Format Definitions.py
@@ -0,0 +1,281 @@
+# File compatibility for file formats
+# Currently only keeps them as values, and applies an outputted name value to them
+# They don't apply to the system all the way yet
+.0 == str
+.0 == ("0 File")
+.01 == str
+.01 == ("01 File")
+.02 == str
+.02 == ("02 File")
+.03 == str
+.03 == ("03 File")
+.04 == str
+.04 == ("04 File")
+.05 == str
+.05 == ("05 file")
+.06 == str
+.06 == ("06 File") 
+.07 == str
+.07 == ("07 File")
+.08 == str
+.08 == ("08 File")
+.09 == str
+.09 == ("09 File")
+.1 == str
+.1 == ("1 File")
+.10 == str
+.10 == ("10 File")
+.11 == str
+.11 == ("11 File")
+.12 == str
+.12 == ("12 File")
+.13 == str
+.13 == ("13 File")
+.14 == str
+.14 == ("14 File")
+.15 == str
+.15 == ("15 File")
+.16 == str
+.16 == ("16 File")
+Z3GP = str
+Z3GP == ("3GP File")
+A = str
+A == ("A File")
+AA = str
+AA == ("AA File")
+AAC = str
+AAC == ("AAC File")
+AX = str
+AX == ("AX File")
+AAX = str
+AAX == ("AAX File")
+AMR = str
+AMR == ("AMR File")
+APE = str
+APE == ("APE File")
+AVI = str
+AVI == ("AVI File (Audio Video Interleave File)")
+APK = str
+APK == ("APK File (Android Package File)")
+AU = str
+AU == ("AU File (Audacity File)")
+AUP = str
+AUP == ("AUP File (Audacity Project File)")
+AVC = str
+AVC == ("AVC File (KasperSky Virus Database File)")
+ASF = str
+ASF == ("ASF File")
+AMV = str
+AMV == ("AMV File")
+U = str
+U == ("U File")
+B = str
+B == ("B File")
+BIN = str
+BIN == ("BIN File (Binary Media File)")
+BMP = str
+BMP == ("BMP File (Bitmap Image File)")
+BAT = str
+BAT == ("BAT File (Batch File)")
+C = str
+C == ("C File")
+CAB = str
+CAB == ("CAB File (Cabinet File)")
+CHM = str
+CHM == ("CHM File (Compiled HTML File)")
+D = str
+D == ("D File")
+DB = str
+DB == ("DB File (DataBase File")
+DAT = str
+DAT == ("DAT File (Data File)")
+DMG = str
+DMG == ("DMG File (MacOS Disk Image File)")
+EXE = str
+EXE == ("EXE File (Windows Executable File)")
+E = str
+E == ("E File")
+F = str
+F == ("F File")
+FLV = str
+FLV == ("FLV File (Flash Video File)")
+G = str
+G == ("G File")
+GIF = str
+GIF == ("GIF File (Graphics Interchange File)")
+GIFV = str
+GIFV == ("GIFV file (HTML5 MPEG-4 File)")
+GIFC200 = str
+GIFC200 == ("GIF_C200 File (Google Chrome GIF (Graphics Interchange Format File) File)")
+H = str
+H == ("H File")
+HTM = str
+HTM == ("HTM File (HyperText Markup Language)")
+HTML = str
+HTML == ("HTML File (HTML Document")
+i = str
+i == ("i File")
+INI = str
+INI == ("INI File (Initiative file File)")
+ISO = str
+ISO == ("ISO File (Optical Disk Image)")
+INF = str
+INF == ("INF File (Information File)")
+IMAGE = str
+IMAGE == ("IMAGE File")
+IMG = str
+IMG == ("IMG File")
+INK = str
+INK == ("INK File (Shortcut File)")
+J = str
+J == ("J File")
+JS = str
+JS == ("JS File (Javascript File)")
+JAR = str
+JAR == ("JAR File (Java Archive File)")
+JPE = str
+JPE == ("JPE File (JPE File Interchange Format)")
+JPG = str
+JPG == ("JPG File (JPG File interchange Format)")
+JPEG = str
+JPEG == ("JPEG File (JPEG File Interchange Format)")
+JFIF = str
+JFIF == ("JFIF File (JFIF File Interchange Format)")
+K = str
+K == ("K File")
+L = str
+L == ("L File")
+LOCK = str
+LOCK == ("LOCK File")
+M = str
+M == ("M File")
+M4A = str
+M4A == ("M4A File (MPEG-4 Audio File)")
+MP4 = str
+MP4 == ("MP4 File (MPEG-4 Video File)")
+MKV = str
+MKV == ("MKV (Matroska Multimedia Container File)")
+MID = str
+MID == ("MID File (MIDI File) (Musical Instrument Digital Interface File)")
+MO = str
+MO == ("MO File")
+MOV = str
+MOV == ("MOV File (Quicktime Movie File)")
+O = str
+O == ("O File")
+OGG = str
+OGG == ("OGG File (Vorbis Compressed Audio File)")
+ODS = str
+ODS == ("ODS File (Open Source Spreadsheet File)")
+ODX = str
+ODX == ("ODX File")
+P = str
+P == ("P File")
+PY = str
+PY == ("PY File (Python File)")
+PYW = str
+PYW == ("PYW File (Python Windows File)")
+PDF = str
+PDF == ("PDF File (Portable Document Format)")
+PNG = str
+PNG == ("PNG File (Portable Network Graphics File)")
+PSD = str
+PSD == ("PSD File (PhotoShop Document File)")
+Q = str
+Q == ("Q File")
+QT = str
+QT == ("QT File (Quicktime Framework Format)")
+R = str
+R == ("R File")
+RPM = str
+RPM == ("RPM File (Redhat Package Management File)")
+RAR = str
+RAR == ("RAR File (Roshal Archival Compressed File)")
+RAR4 = str
+RAR4 == ("RAR File (Roshal Archival Compressed File)")
+RTF = str
+RTF == ("RTF File (Rich Text Format Document File)")
+S = str
+S == ("S File")
+SLN = str
+SLN == ("SLN File (Microsoft Visual Studio Structure File)")
+TXT = str
+TXT == ("TXT File (Plain Text Document)")
+SB2 = str
+SB2 == ("SB2 File(Scratch Blockly 2.0 File)")
+SVG = str
+SVG == ("SVG File (Scalable Vector Graphics File)")
+SIG = str
+SIG == ("SIG File")
+SYS = str
+SYS == ("U File")
+T = str
+T == ("T File")
+TAR = str
+TAR == ("TAR File (Tape Archive File)")
+TIF = str
+TIF == ("TIF File (Tagged Image File Format)")
+TIFF = str
+TIFF == ("TIFF File (Tagged Image File Format)")
+TTF = str
+TTF == ("TTF File (TrueType Font File)")
+U = str
+U == ("U File")
+URL = str
+URL == ("URL File (Web URL shortcut File)")
+V = str
+V == ("V File")
+VBS = str
+VBS == ("VBS File (Visual Basic Script File)")
+VHD = str
+VHD == ("VHD File (Virtual Hard Disk File)")
+VDI = str
+VDI == ("VDI File (Virtualbox Disk Image)")
+VMDK = str
+VMDK == ("VMDK File (Virtual Machine Disk File)")
+VOB = str
+VOB == ("VOB File")
+VFD = str
+VFD == ("VFD File (Virtual Floppy Disk Image 1.44MB)")
+W = str
+W == ("W File")
+WEBM = str
+WEBM == ("WEBM File (HTML5 Video Streaming File)")
+WMV = str
+WMV == ("WMV File (Windows Media Video File)")
+WLMP = str
+WLMP == ("WLMP File (Windows Movie Maker 2012 Project File)")
+WMMP = str
+WMMP == ("WMMP File (Windows Movie Maker Project File)")
+WMA = str
+WMA == ("WMA File")
+WAV = str
+WAV == ("WAV File")
+U = str
+U == ("U File")
+X = str
+X == ("X File")
+XY = str
+XY == ("XY File (XYWrite file)")
+Y = str
+Y == ("Y File")
+Z = str
+Z == ("Z File")
+Z64 = str
+Z64 == ("Z64 File (Nintendo 64 Patch File)")
+ZIP = str
+ZIP == ("ZIP File (compressed file format)")
+
+print ("===")
+print ("Inside Preview:")
+print ("_______________________________________")
+print ("File definitions for 100+ file types")
+print ("Contains: Definitions, Comments, File Info")
+print ("Language: Python 3.6.4")
+print (" ") 
+testconfirm = int(input("This is a system file that is not meant to be launched through the MEDOS terminal. This program holds the strings and values to ensure that files are compatible with the system. The file can be modified through the shell, but not MEDOS. This program cannot run, and is not designed to run. Press ENTER to quit"))
+if testconfirm == 1:
+    print ("Leaving")
+    print ("Closing out of driver window")
+else:
+    print ("Exiting")   
