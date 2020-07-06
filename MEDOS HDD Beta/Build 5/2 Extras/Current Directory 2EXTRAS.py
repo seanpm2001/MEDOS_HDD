7e2ad4f8d922eedc5c5bdde96a93823829ed41c0
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-06 15:42:49 -0700

    7 files
    7 files at the root of the /2 Extras/ directory, in Plain text (5x *.txt) and Python (2x *.py) formats.

---
+++ MEDOS HDD Beta/Build 5/2 Extras/Current Directory 2EXTRAS.py
@@ -0,0 +1,29 @@
+in1 = str
+print ("Current directory")
+print ("Type 'ENTER' to view")
+in1 = str(input("FC://"))
+if in1 == ("ENTER"):
+	print ("Contents of 2EXTRAS")
+	print ("System")
+	print (">Extras")
+	print (">Extras>DOSTOP 1.0.png  | 12,371 bytes (12.37 Kilobytes)")
+	print (">Extras>Logo.bmp  | 5,318 bytes (5.31 Kilobytes)")
+	print (">Extras>MEDOS Build 2 VS MSDOS 10.0.png  | 95,869 bytes (95.86 Kilobytes)")
+	print (">Extras Total size = 113,558 bytes (113.55 kilobytes) 3 files + 0 folders = 3 items")
+	print (">1.03.txt  | 154 bytes")
+	print (">DNA circle.txt  | 163 bytes")
+	print (">Set up a connection.txt  | 2,379 bytes (2.37 Kilobytes)")
+	print (">Setup.txt  | 29,908 bytes (29.90 Kilobytes)")
+	print (">Source.txt  | 15,822 bytes (15.82 Kilobytes)")
+	print (">Test.py  | 11,515 bytes (11.51 Kilobytes)")
+	print ("Current Directory 2EXTRAS.PY  | 1,358 bytes (1.31 Kilobytes)")
+	print ("> Total size: 61,299 bytes (61.29 Kilobytes) 6 files + 0 folders = 7 items")
+	print ("-- Total size of 2EXTRAS: 174,857 bytes (174.85 Kilobytes) 10 files + 1 folder = 11 items")
+	print (in1)
+testconfirm = int(input("Did the command info look right? type 1 to confirm, type anything else and/or press enter to quit "))
+if testconfirm == 1:
+    print ("Test successful!")
+    print ("Shutting down, please wait")
+else:
+    print ("Shutting down")
+   
